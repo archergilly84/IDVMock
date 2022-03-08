b130fe248b4a79a1a6d2a5837eebd7c15f7032ca
david.gilligan <david.gilligan@engineering.digital.gov.uk>
2022-03-08 16:42:47 +0000

    added stuff

--- index.js
+++ index.js
@@ -1,5 +1,18 @@
 const express = require("express");
 const fs = require('fs');
+const { Pool } = require("pg");
+
+
+
+//Postgres setup
+const pool = new Pool(
+    {
+        connectionString : "postgres://fdhjglpkvtwsvo:7ed4533c9afa1d3c11adf1ede368d53bca5597982f856b2f6c6c2ba00a98ec3b@ec2-54-77-182-219.eu-west-1.compute.amazonaws.com:5432/d4l78537dgpkfu",
+        ssl: {
+            rejectUnauthorized: false
+        }
+    }
+)
 
 const app = express();
 
@@ -13,45 +26,59 @@ app.listen(port);
 //To make the application deal with JSON
 app.use(express.json());
 
-const matching = ["Enter CLI telephone number", "Date of Birth", "telephone number", "postcode", "nino"];
+const challenges = ["Enter CLI telephone number", "Date of Birth", "telephone number", "postcode", "nino",
+"CIS_Home_Telephone_Number", "CIS_Mobile_Telephone_Number", "CIS_Benefits", "CIS_Childs_DOB", "CIS_Partners_NINO",
+"ESA_Last_Payment_Amount", "ESA_Last_Payment_Date","ESA_Pay_Day", "ESA_Bank_Account", "ESA_Sort_Code",
+"PIP_Last_Payment_Amount", "PIP_Last_Payment_Date","PIP_Pay_Day", "PIP_Bank_Account", "PIP_Sort_Code"];
 const cis_challenges = ["CIS_Home_Telephone_Number", "CIS_Mobile_Telephone_Number", "CIS_Benefits", "CIS_Childs_DOB", "CIS_Partners_NINO"];
 const esa_challenges = ["ESA_Last_Payment_Amount", "ESA_Last_Payment_Date","ESA_Pay_Day", "ESA_Bank_Account", "ESA_Sort_Code"];
 const pip_challenges = ["PIP_Last_Payment_Amount", "PIP_Last_Payment_Date","PIP_Pay_Day", "PIP_Bank_Account", "PIP_Sort_Code"];
 
-const users = [{
-    guid: "",
-    dob: "22-Jun-1986",
-    phone: "07123456789",
-    cis_benefit: "67",
-    cis_childs_name: "Marge Smith",
-    esa_bank_details: "0000",
-    esa_pay_day: "Wed",
-    pip_component: "10001, 10010",
-    pip_last_pay_date: "22-Dec-2022"   
-},{
-    guid: "",
-    dob: "02-Feb-1978",
-    phone: "07123456711",
-    cis_home_phone: "01112435675",
-    cis_benefit: "675",
-    esa_lastpayment_amount: "12345",
-    esa_pay_day: "Fri",
-    pip_lastpayment_amount: "10001",
-    pip_lastpayment_date: "22-Dec-2022"   
-}]
+// const users = [{
+//     guid: "",
+//     dob: "22-Jun-1986",
+//     phone: "07123456789",
+//     cis_benefit: "67",
+//     cis_childs_name: "Marge Smith",
+//     esa_bank_details: "0000",
+//     esa_pay_day: "Wed",
+//     pip_component: "10001, 10010",
+//     pip_last_pay_date: "22-Dec-2022"   
+// },{
+//     guid: "",
+//     dob: "02-Feb-1978",
+//     phone: "07123456711",
+//     cis_home_phone: "01112435675",
+//     cis_benefit: "675",
+//     esa_lastpayment_amount: "12345",
+//     esa_pay_day: "Fri",
+//     pip_lastpayment_amount: "10001",
+//     pip_lastpayment_date: "22-Dec-2022"   
+// }]
 
-const matched = (phone , dob) => {
-
-    //const challenges = [[cis_challenges],[esa_challenges],[pip_challenges]];
-
-    let cis_question = cis_challenges[Math.random() * cis_challenges.length];
+const matched = (phone, dob, postcode) => {
+    const matchedUserArray = [];
     
+    const dbUserArray = []; //all users from DB
 
-    for(user in users){
-        if(user.phone === phone && user.dob === dob){
-            
+    //Access matching table and extract data
+
+
+    if(postcode != undefined){
+       for(user in dbUserArray){
+           if(user.postcode === postcode && user.dob === dob && user.phone === phone){
+               matchedUserArray.push(user);
+           }
+       }
+    } else {
+        for(user in dbUserArray){
+            if(user.dob === dob && user.phone === phone){
+                matchedUserArray.push(user);
+            }
         }
     }
+    
+    return matchedUserArray;
 }
 
 
@@ -91,12 +118,15 @@ app.post("/AMTree", (req, res) => {
         res.status(200).send(response);
     } else {
 
-        const userFile = fs.readFileSync("database.json");
-        let user = JSON.parse(userFile);
-        
+        //Run query to get all callers that match. 
+
         let prompt = req.body.callbacks[0].output[0].value;
         
-        if(matching.includes(prompt)){
+        if(typeof prompt !== 'string'){
+            prompt = prompt.fieldId;
+        } 
+        
+        if(challenges.includes(prompt)){
             switch(prompt){
                  
                 case "Enter CLI telephone number":
@@ -105,7 +135,7 @@ app.post("/AMTree", (req, res) => {
                         cli: req.body.callbacks[0].input[0].value
                     } 
 
-                    fs.writeFileSync("./database.json", JSON.stringify(cli, null, 2))
+                    //Add cli to the matching table.
 
                     response = {
                         "callbacks": [{
@@ -126,10 +156,8 @@ app.post("/AMTree", (req, res) => {
                     break;
 
                 case "Date of Birth":
-                      
-                    fs.writeFileSync("./database.json", {
-                        DOB: req.body.callbacks[0].input[0].value
-                    })
+                
+                    let challengeQuestion = cis_challenges[Math.random() * cis_challenges.length];
                     
                     response = {
                         "cookie": "dthamlbcookie=01; Path=/; Secure; HttpOnly; SameSite=none",
@@ -147,8 +175,81 @@ app.post("/AMTree", (req, res) => {
                         "authId":
                         "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJhdXRoSW5kZXhWYWx1ZSI6IlRJRFYiLCJvdGsiOiJwbmpnYjlxaXM4bG44aWFiYm02ZjdnMHEwYSIsImF1dGhJbmRleFR5cGUiOiJzZXJ2aWNlIiwicmVhbG0iOiIvQ2l0aXplbnMvVElEViIsInNlc3Npb25JZCI6InVzZGh0WG5wc1A5bWh6dWVYcnFwS2VHdUE3QS4qQUFKVFNRQUNNRElBQWxOTEFCeFlhRXhpVW1sR2VVVjViamhMY25WRFltUjJRakJGWlcwcmNrazlBQVIwZVhCbEFBaERWRk5mUVZWVVNBQUNVekVBQWpBeCoiLCJleHAiOjE2MzcwODMwMTgsImlhdCI6MTYzNzA4MjExOH0.1soDjOUXL2H-dUTxw59kpUSDTfoJJtIIgfjt_R9BaRE"
                     }
+                      
+                    let dob = {
+                        dob: req.body.callbacks[0].input[0].value
+                    }
+
+                    //Add the DOB matching table.
+                    //Extract CLI
+                    let matchedSize = matching(cli,dob).length;
+
+                    //Find available challenges from matched user within users table. 
+
+                    if(matchedSize === 1){
+                        response.callbacks[0].output[0].value = challengeQuestion;
+                    } else if(matchedSize < 1) {
+                        //TODO Error Response from ID&V App.
+                    } else {
+                        response.callbacks[0].output[0].value = "postcode";
+                    }
+                    break;
+
+                case "postcode":
+
+                    let matchedSize = matching(cli,dob,postcode).length;
+                    let challengeQuestion = cis_challenges[Math.random() * cis_challenges.length];
+                   
+                    response = {
+                        "cookie": "dthamlbcookie=01; Path=/; Secure; HttpOnly; SameSite=none",
+                        "callbacks": [{
+                            "output": [{
+                                "name": "prompt",
+                                "value": ""
+                            }],
+                            "input": [{
+                                "name": "IDToken1",
+                                "value": ""
+                            }],
+                            "type": "NameCallback"
+                        }],
+                        "authId":
+                        "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJhdXRoSW5kZXhWYWx1ZSI6IlRJRFYiLCJvdGsiOiJwbmpnYjlxaXM4bG44aWFiYm02ZjdnMHEwYSIsImF1dGhJbmRleFR5cGUiOiJzZXJ2aWNlIiwicmVhbG0iOiIvQ2l0aXplbnMvVElEViIsInNlc3Npb25JZCI6InVzZGh0WG5wc1A5bWh6dWVYcnFwS2VHdUE3QS4qQUFKVFNRQUNNRElBQWxOTEFCeFlhRXhpVW1sR2VVVjViamhMY25WRFltUjJRakJGWlcwcmNrazlBQVIwZVhCbEFBaERWRk5mUVZWVVNBQUNVekVBQWpBeCoiLCJleHAiOjE2MzcwODMwMTgsImlhdCI6MTYzNzA4MjExOH0.1soDjOUXL2H-dUTxw59kpUSDTfoJJtIIgfjt_R9BaRE"
+                    }
+
+                    if(matchedSize === 1){
+                        response.callbacks[0].output[0].value = challengeQuestion;
+                    } else if(matchedSize < 1) {
+                        //TODO Error Response from ID&V App.
+                    } else {
+                        //TODO Error 
+                    }
+                    
+                    break;
+
+                case "cis_benefit":
+
+                    response = {
+                        "cookie": "dthamlbcookie=01; Path=/; Secure; HttpOnly; SameSite=none",
+                        "callbacks": [{
+                            "output": [{
+                                "name": "prompt",
+                                "value": ""
+                            }],
+                            "input": [{
+                                "name": "IDToken1",
+                                "value": ""
+                            }],
+                            "type": "NameCallback"
+                        }],
+                        "authId":
+                        "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJhdXRoSW5kZXhWYWx1ZSI6IlRJRFYiLCJvdGsiOiJwbmpnYjlxaXM4bG44aWFiYm02ZjdnMHEwYSIsImF1dGhJbmRleFR5cGUiOiJzZXJ2aWNlIiwicmVhbG0iOiIvQ2l0aXplbnMvVElEViIsInNlc3Npb25JZCI6InVzZGh0WG5wc1A5bWh6dWVYcnFwS2VHdUE3QS4qQUFKVFNRQUNNRElBQWxOTEFCeFlhRXhpVW1sR2VVVjViamhMY25WRFltUjJRakJGWlcwcmNrazlBQVIwZVhCbEFBaERWRk5mUVZWVVNBQUNVekVBQWpBeCoiLCJleHAiOjE2MzcwODMwMTgsImlhdCI6MTYzNzA4MjExOH0.1soDjOUXL2H-dUTxw59kpUSDTfoJJtIIgfjt_R9BaRE"
+                    }
+
                     break;
             }
+
+            
         }
 
         res.status(200).send(response);
